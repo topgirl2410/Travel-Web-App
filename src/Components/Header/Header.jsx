b0f03d117aa1b690ec08b740153802f7fe0d67b3
topgirl2410 <maria.topgirl82@gmail.com>
2024-01-18 23:54:13 +0100

    creando el header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,43 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className='Header'>Header</div>
+        <div className='Navbar'>
+            <div className="logoDiv">
+                <h2 className='logo'>Yago</h2>
+            </div>
+
+            <div className="menu">
+                <div className="lists flex">
+                    <li>
+                        <a href="#">Destination</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Hotels</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Flights</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Booking</a>
+                    </li>
+                </div>
+            </div>
+
+            <div className="btns flex">
+                <button className='btn'>Login</button>
+                <button className='btn'>SingUp</button>
+                <select className='lang'>
+                    <option value="">EN</option>
+                    <option value="">FR</option>
+                    <option value="">SP</option>
+                </select>
+            </div>
+
+
+        </div>
     )
 }
 
