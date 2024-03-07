4ef818d5b53259a1b7e722b5a199c80f5b252891
topgirl2410 <maria.topgirl82@gmail.com>
2024-03-07 13:50:31 +0100

    destinations

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import './App.css'
 import Header from './Components/Header/Header'
 import Home from './Components/Home/Home'
 import Categories from './Components/Categories/Categories'
-{/*import Destinations from './Components/Destinations/Destinations'
+import Destinations from './Components/Destinations/Destinations'
 import Guide from './Components/Guide/Guide'
+{/*
 import Sponsors from './Components/Sponsors/Sponsors'
 import Subscribe from './Components/Subscribe/Subscribe'
 import Footer from './Components/Footer/Footer'*/}
@@ -16,8 +17,9 @@ function App() {
       <Header />
       <Home />
       <Categories />
-      {/* <Destinations />
+      <Destinations />
       <Guide />
+      {/* 
       <Sponsors />
       <Subscribe />
   <Footer />*/}
--- src/Components/Destinations/Destinations.jsx
+++ src/Components/Destinations/Destinations.jsx
@@ -1,9 +1,42 @@
 
 import './Destinations.css'
 
+// Import Icon
+import { FaLocationArrow } from "react-icons/fa";
+
+// Import Images 
+import imageOne from '../../assets/roma.jpg'
+
 const Destinations = () => {
     return (
-        <div className='Destinations'>Destinations</div>
+        <div className='Destinations container section'>
+            <div className="secHeader">
+                <span>Top Selling</span>
+                <h2>Top Destinations</h2>
+            </div>
+
+            <div className="secContainer grid">
+                <div className="singleCard grid">
+                    <div className="imgDiv">
+                        <img src={imageOne} alt="destination one" />
+                    </div>
+
+                    <div className="infoDiv">
+                        <div className="upperSpan flex">
+                            <span className='location'>Rome, Italy</span>
+                            <span className='amount'>1500€</span>
+                        </div>
+                        <div className="lowerSpan flex">
+                            <FaLocationArrow className='icon' />
+
+                            <span className="distance">
+                                10 Days Trip
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
